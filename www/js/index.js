4900f95014151c6c216a1e1d725294366be30026
jglazzard <glaj1_14@CH1001-025.uni.worc.ac.uk>
2015-10-19 10:42:22 +0100

    no device ready

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var resumed_count = 0;
 var launched_count = 0;
 
     function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
+        //document.addEventListener("deviceready", onDeviceReady, false);
 		//console.log("device ready");
 		var key = "pen";
 		var value = "blue";
